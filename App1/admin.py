114b425780f0a747c3386ef303b24703d1d5b3f2
agustinmassa <agustinmassa8@gmail.com>
2023-05-08 09:15:10 -0300

    models listos

---
+++ App1/admin.py
@@ -0,0 +1,9 @@
+
+# Register your models here.
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Curso)
+admin.site.register(Estudiante)
+admin.site.register(Profesor)
